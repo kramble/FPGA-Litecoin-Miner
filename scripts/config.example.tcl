72fc763425cc97d3044b8eafd00c49bbd452c7e0
kramble <kramjelly@gmail.com>
2013-07-22 16:15:30 +0100

    First commit

---
+++ scripts/config.example.tcl
@@ -0,0 +1,7 @@
+# Edit this with your details and save as config.tcl
+# They are the same worker server, user/pass you use
+# with other mining software.
+#
+set url "http://mining-foreman.org:10341"
+set userpass "Username:Password"
+
